42f0b415249a40f3f800dbb15a6f264755e998ea
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-10-22 23:48:05 +0530

    service api Gateway

---
+++ service-gateway/src/test/java/com/pk/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
